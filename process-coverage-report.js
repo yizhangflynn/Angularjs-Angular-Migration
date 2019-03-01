6e442353926a5cf2c9bd9bbda1f1a67791860dae
Yi Zhang <Yi.Zhang@flynncompanies.com>
2019-03-01 10:50:53 -0500

    test azure ci

--- process-coverage-report.js
+++ process-coverage-report.js
@@ -4,7 +4,7 @@ const overrideCss = '.status-line { clear:both;} ' +
     '.coverage .line-count, .coverage .line-coverage, ' +
     '.coverage .text .prettyprint {font-size:12px !important; ' +
     'line-height:1.2 !important;font-family:Consolas, "Liberation Mono", Menlo, Courier, monospace !important;}' +
-    '.coverage .line-count{max-width:40px;padding-right:25px !important;} ' +
+    '.coverage .line-count{max-width:40px;padding-right:25px !important;display:block !important;} ' +
     '.coverage .line-coverage{max-width:45px;}' +
     '.coverage .line-coverage .cline-any{padding-right:25px !important;}' +
     '.coverage-summary{font-size:small;}';
